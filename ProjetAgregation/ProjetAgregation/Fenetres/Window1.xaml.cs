6069865b960caa2e0fa5d12f42fb8e55acb74766
samwil0 <samwil09@hotmail.ca>
2018-11-13 17:26:34 -0500

    Les Fenêtres fonctionnes parfaitement.

--- ProjetAgregation/ProjetAgregation/Fenetres/Window1.xaml.cs
+++ ProjetAgregation/ProjetAgregation/Fenetres/Window1.xaml.cs
@@ -19,74 +19,77 @@ namespace ProjetAgregation.Fenetres
     /// </summary>
     public partial class Window1 : Window
     {
-
-
-
         public Window1()
         {
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
-
-
-
-
-
         //Fonctions reserver pour changer de fenetre
         private void Acueil_Click(object sender, RoutedEventArgs e)
         {
             Window1 acueil = new Window1();
             acueil.Show();
+            App.Current.MainWindow = acueil;
             this.Close();
         }
         private void Amis_Click(object sender, RoutedEventArgs e)
         {
             Window2 amis = new Window2();
             amis.Show();
+            App.Current.MainWindow = amis;
             this.Close();
         }
         private void Email_Click(object sender, RoutedEventArgs e)
         {
             Window3 email = new Window3();
             email.Show();
+            App.Current.MainWindow = email;
             this.Close();
         }
         private void Graph_Click(object sender, RoutedEventArgs e)
         {
             Window4 graph = new Window4();
             graph.Show();
+            App.Current.MainWindow = graph;
             this.Close();
         }
         private void News_Click(object sender, RoutedEventArgs e)
         {
             Window5 news = new Window5();
             news.Show();
+            App.Current.MainWindow = news;
             this.Close();
         }
         private void Chat_Click(object sender, RoutedEventArgs e)
         {
             Window6 chat = new Window6();
             chat.Show();
+            App.Current.MainWindow = chat;
             this.Close();
         }
         private void Pokemon_Click(object sender, RoutedEventArgs e)
         {
             Window7 pokemon = new Window7();
             pokemon.Show();
+            App.Current.MainWindow = pokemon;
             this.Close();
         }
         private void Crypto_Click(object sender, RoutedEventArgs e)
         {
             Window8 crypto = new Window8();
             crypto.Show();
+            App.Current.MainWindow = crypto;
             this.Close();
         }
 
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
-            App.Current.MainWindow.Show();
+            MainWindow menu = new MainWindow();
+            menu.Show();
+            App.Current.MainWindow = menu;
             this.Close();
         }
+
 
     }
 }
--- ProjetAgregation/ProjetAgregation/MainWindow.xaml.cs
+++ ProjetAgregation/ProjetAgregation/MainWindow.xaml.cs
@@ -28,60 +28,62 @@ namespace ProjetAgregation
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged()
-        {
-
-        }
-
-
         //Fonctions reserver pour changer de fenetre
         private void Acueil_Click(object sender, RoutedEventArgs e)
         {
             Window1 acueil = new Window1();
             acueil.Show();
-            this.Hide();
+            App.Current.MainWindow = acueil;
+            this.Close();
         }
         private void Amis_Click(object sender, RoutedEventArgs e)
         {
             Window2 amis = new Window2();
             amis.Show();
-            this.Hide();
+            App.Current.MainWindow = amis;
+            this.Close();
         }
         private void Email_Click(object sender, RoutedEventArgs e)
         {
             Window3 email = new Window3();
             email.Show();
-            this.Hide();
+            App.Current.MainWindow = email;
+            this.Close();
         }
         private void Graph_Click(object sender, RoutedEventArgs e)
         {
             Window4 graph = new Window4();
             graph.Show();
-            this.Hide();
+            App.Current.MainWindow = graph;
+            this.Close();
         }
         private void News_Click(object sender, RoutedEventArgs e)
         {
             Window5 news = new Window5();
             news.Show();
-            this.Hide();
+            App.Current.MainWindow = news;
+            this.Close();
         }
         private void Chat_Click(object sender, RoutedEventArgs e)
         {
             Window6 chat = new Window6();
             chat.Show();
-            this.Hide();
+            App.Current.MainWindow = chat;
+            this.Close();
         }
         private void Pokemon_Click(object sender, RoutedEventArgs e)
         {
             Window7 pokemon = new Window7();
             pokemon.Show();
-            this.Hide();
+            App.Current.MainWindow = pokemon;
+            this.Close();
         }
         private void Crypto_Click(object sender, RoutedEventArgs e)
         {
             Window8 crypto = new Window8();
             crypto.Show();
-            this.Hide();
+            App.Current.MainWindow = crypto;
+            this.Close();
         }
 
     }
